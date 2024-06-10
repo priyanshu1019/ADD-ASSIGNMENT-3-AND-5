d99f6bd6fa13073263fa40bbc2452bc10f0a3417
priyanshu1019 <goutampriyanshu27@gmail.com>
2024-06-10 13:28:01 +0530

    using kotlin is easy

--- app/src/main/java/com/example/class1/Student.kt
+++ app/src/main/java/com/example/class1/Student.kt
@@ -1,37 +1,3 @@
-package com.example.class1;
+package com.example.class1
 
-public class Student {
-    String name;
-    String reg;
-    int age;
-
-    public Student(String name, String reg, int age) {
-        this.name = name;
-        this.reg = reg;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getReg() {
-        return reg;
-    }
-
-    public void setReg(String reg) {
-        this.reg = reg;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-}
+class Student(var name: String, var reg: String, var age: Int)
---
+++ app/src/main/java/com/example/class1/kotlinex/StudentUse.kt
@@ -0,0 +1,8 @@
+package com.example.class1.kotlinex
+
+import com.example.class1.Student
+
+fun main() {
+    var myStudent = Student("prianshu" , "21BCE0400" , 21)
+    println("name is "+myStudent.name)
+}
